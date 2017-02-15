1ac5a9520ac9982af0ac60ab7b7ee78eebeec69f
yoannma <yoann.mallemanche@gmail.com>
2017-02-15 18:10:21 +0100

    i did thing, don't remember

--- app/app.js
+++ app/app.js
@@ -2,13 +2,17 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+    'ngRoute',
+    'nix.api',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+]).config([
+    '$locationProvider', '$routeProvider', 'nixApiProvider', function ($locationProvider, $routeProvider, nixApiProvider) {
+        $locationProvider.hashPrefix('!');
+        
+        $routeProvider.otherwise({ redirectTo : '/view1' });
+        
+        nixApiProvider.setApiCredentials('a1dae454', '11d2ef2c51fc274cf359a274a1cc2df0');
+    }
+]);
